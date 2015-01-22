d39ece99947148d9634440fc5e7bcf590196f31a
jdpepperman <jdpepperman@crimson.ua.edu>
2015-01-22 00:27:29 -0600

    Sunday Bloody Sunday
    Started the drum intro of Sunday Bloody Sunday to practice what we
    learned in class today. There seems to be a short pause in between each
    measure though.

---
+++ Resources/sundayBloodySunday/sundayBloodySunday.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "/usr/local/include/songlib/util.h"
+#include "/usr/local/include/songlib/songlib.h"
+
+#define dir "/usr/local/share/samples/piano/"
+#define base "bright_"
+
+/* change PROGRAM_NAME and PROGRAM_VERSION appropriately */
+
+char *PROGRAM_NAME = "sundayBloodySunday";
+char *PROGRAM_VERSION = "0.01";
+
+int
+main()
+{
+	int spot;
+
+	songInit();
+
+	//setSlopTiming(W);	
+
+	setTempo(100);
+	setTime(4,4);
+	setStride(0.05);
+	setSustain(0.99995);
+	setAmplitude(0.4);
+
+	openOutput("sundayBloodySunday.rra",0,0);
+
+	spot = getLocation();
+	drumkitSnare(1,1,	"----------------------------x-x-",SX); 
+	
+	spot = getLocation();
+	drumkitHHClosed(1,1,	"-----x-x---x-x-x-x-x-x-x-x-x-----","-x-x-x-x-x-x-----x-x-x-x-x-x-----",SX); setLocation(spot);
+	drumkitSnare(1,1,	"-x-x-----x-------------------x-x-","-------------x-x-------------x-x-",SX); setLocation(spot);
+	drumkitKick(1,3,	"-x-------x-------x-------x-------","-x-------x-------x-------x-------",SX); //that 3 changes amp
+
+	closeOutput();
+
+	return 0;
+}
