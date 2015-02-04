33cb6461c1210955a7020c3578a64541e3b80807
jdpepperman <jdpepperman@crimson.ua.edu>
2015-02-04 13:39:24 -0600

    Trying to get drums to match, I'm about to try to change timing

---
+++ Project/drums.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "/usr/local/include/songlib/util.h"
+#include "/usr/local/include/songlib/songlib.h"
+
+#define dir "/usr/local/share/samples/piano/"
+#define base "bright_"
+
+/* change PROGRAM_NAME and PROGRAM_VERSION appropriately */
+
+char *PROGRAM_NAME = "drums";
+char *PROGRAM_VERSION = "0.01";
+
+int
+main()
+{
+	int spot;
+
+	songInit();
+
+	//setSlopTiming(W);	
+
+	setTempo(200);
+	setTime(4,4);
+	setStride(0.05);
+	setSustain(0.99995);
+	setAmplitude(0.4);
+	openOutput("drums.rra",0,0);
+
+	int i;
+	for (i = 0; i < 4; i++){
+		spot = getLocation();
+		drumkitHHClosed(1,1,	"----------------",SX); setLocation(spot);
+		drumkitSnare(1,1,	"-x--x----x--x---",SX); setLocation(spot);
+		drumkitKick(1,3,	"x-xx------xx---x",SX); //that 3 changes amp
+	}
+	closeOutput();
+
+	return 0;
+}
