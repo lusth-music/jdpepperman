728b871d4cfd64b79253e43da6de6bf0c3d9ed1f
jdpepperman <jdpepperman@crimson.ua.edu>
2015-02-04 13:23:21 -0600

    renamed stuff for easier mixing

---
+++ Project/harmonica.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "/usr/local/include/songlib/util.h"
+#include "/usr/local/include/songlib/songlib.h"
+
+
+#define dir "/usr/local/share/samples/"
+//#define base "piano/bright_"
+#define base "harmonica/pocket_"
+
+/* change PROGRAM_NAME and PROGRAM_VERSION appropriately */
+
+char *PROGRAM_NAME = "harmonica";
+char *PROGRAM_VERSION = "0.01";
+
+static int last = 0;
+
+int
+main()
+{
+	int instrument;
+	int octave = 4;
+
+	songInit();
+
+	instrument = readScale(dir,base);
+
+	setTempo(200);
+	setTime(3,4);
+	setStride(0.05);
+	//setSustain(0.99995);
+	setAmplitude(0.3);
+
+	openOutput("harmonica.rra",0,0);
+
+	int i;
+//	for (i=0; i<2; i++) {
+//		setSustain(0.5);
+//		b(5,H,instrument,octave-1, "x---", "-x--", "--x-", SX);
+//		b(5,H,instrument,octave-1, "---x", "x---", "--x-", SX);
+//		//b(chord, duration[whole(w), half(h), quarter(q), eighth(i), sixteenth(s), thirtysecond(t)], instrument, octave, which notes to play, SX is the terminator)
+//		setSustain(0.99965);
+//		b(1,W, instrument, octave, "xxxx", "xxxx", "xxxx", SX);
+//		setSustain(0.5);
+//		b(4,H, instrument, octave-1, "x---", "---x", "x---", SX);
+//		b(4,H, instrument, octave-1, "-x--", "--x-", "---x", SX);
+//		//ci[1-6)
+//		setSustain(0.99965);
+//		b(6, W, instrument, octave-1, "x-xx", "---", "x-xx", SX);
+//	}
+	
+	for (i=0; i<2; i++) {
+		b(5,I,instrument,octave-1, "xx-x", SX);
+		b(1,Hd,instrument,octave, "xxx", SX);
+		rest(Q);
+		b(1,I,instrument,octave, "xxx", SX);
+		b(4,Hd,instrument,octave-1, "xxx", SX);
+		rest(Q);
+		b(4,I,instrument,octave-1, "xxx", SX);
+		b(6,Hd,instrument,octave-1, "xxx", SX);
+		rest(Q);
+		b(6,I,instrument,octave-1, "xxx", SX);
+		b(5,Hd,instrument,octave-1, "xxx", SX);
+		rest(Q);
+	}	
+	b(5,I,instrument,octave-1, "xx-x", SX);
+	setSustain(0.99995);
+	cpower2(1, W, instrument, octave);
+
+	closeOutput();
+
+	return 0;
+}
